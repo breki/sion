0ce1290ccbf30bc1b0b993e2d778a40457e26aab
Igor Brejc <igor.brejc@gmail.com>
2024-09-15 12:49:24 +0200

    Fixing some bugs

--- sion/benches/benchmark.rs
+++ sion/benches/benchmark.rs
@@ -2,7 +2,6 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use sion::dem_tile::DemTile;
 use sion::grayscale_bitmap::GrayscaleBitmap;
 use sion::hillshading::igor_hillshading_opt1::hillshade as hillshade_opt1;
-use sion::hillshading::igor_hillshading_opt2::hillshade as hillshade_opt2;
 use sion::hillshading::igor_hillshading_orig::hillshade as hillshade_orig;
 use sion::hillshading::parameters::HillshadingParameters;
 
@@ -30,22 +29,6 @@ fn benchmark_igor_hillshade_opt1(c: &mut Criterion) {
     c.bench_function("igor_hillshade_opt1", |b| {
         b.iter(|| {
             hillshade_opt1(
-                black_box(&dem),
-                black_box(&parameters),
-                black_box(&mut bitmap),
-            )
-        })
-    });
-}
-
-fn benchmark_igor_hillshade_opt2(c: &mut Criterion) {
-    let dem = DemTile::from_file("tests/data/N46E006.hgt");
-    let mut bitmap = GrayscaleBitmap::new(dem.size as u16, dem.size as u16);
-    let parameters = HillshadingParameters::default();
-
-    c.bench_function("igor_hillshade_opt2", |b| {
-        b.iter(|| {
-            hillshade_opt2(
                 black_box(&dem),
                 black_box(&parameters),
                 black_box(&mut bitmap),
