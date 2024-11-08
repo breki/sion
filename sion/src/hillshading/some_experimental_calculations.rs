e4416610520666d89e6bb69729b3a0280b5e4c60
Igor Brejc <igor.brejc@gmail.com>
2024-11-08 05:26:32 +0100

    Added hillshading experimental calculations

--- sion/src/hillshading/some_experimental_calculations.rs
+++ sion/src/hillshading/some_experimental_calculations.rs
@@ -94,107 +94,124 @@ pub fn calculate_aspect_2(p_prime: i16, q_prime: i16) -> f32 {
     }
 }
 
+pub fn diff_between_angles_deg(a: i16, b: i16) -> i16 {
+    let diff = (a - b).abs();
+    if diff > 180 {
+        360 - diff
+    } else {
+        diff
+    }
+}
+
+pub fn hillshading_1(sun_azimuth: i16, slope: i16, aspect: i16) -> i16 {
+    let slope_light_intensity = (90. - slope as f32) / 90.;
+    let aspect_diff = diff_between_angles_deg(aspect, sun_azimuth);
+    let aspect_light_intensity = (180. - aspect_diff as f32) / 180.;
+    let light_intensity = slope_light_intensity * aspect_light_intensity;
+    let color = (255. * light_intensity) as i16;
+    color
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::testing::assert_eq_approx;
 
     #[test]
-    fn test_eastward_slope() {
-        let d = 45;
+    fn test_slope_facing_west() {
         let elevations = Matrix3x3::new([
             1000, 1045, 1090, 1000, 1045, 1090, 1000, 1045, 1090,
         ]);
 
-        let (p, q) = calculate_pq_1(d, &elevations);
-
-        assert_eq!(p, 1.);
-        assert_eq!(q, 0.);
-        assert_eq!(calculate_slope_1(p, q), 45.);
-        assert_eq!(calculate_aspect_1(p, q), 0.);
-
-        let (p_prime, q_prime) = calculate_pq_2(&elevations);
-        assert_eq!(calculate_slope_2(d, p_prime, q_prime), 45.);
-        assert_eq!(calculate_aspect_2(p_prime, q_prime), 0.);
+        assert_calculations(&elevations, 45, 1., 0., 45., 0., 95);
+        assert_calculations(&elevations, 90, 0.5, 0., 26.56505, 0., 136);
     }
 
     #[test]
-    fn test_westward_slope() {
-        let d = 45;
+    fn test_slope_facing_east() {
         let elevations = Matrix3x3::new([
             1090, 1045, 1000, 1090, 1045, 1000, 1090, 1045, 1000,
         ]);
 
-        let (p, q) = calculate_pq_1(d, &elevations);
-
-        assert_eq!(p, -1.);
-        assert_eq!(q, 0.);
-        assert_eq!(calculate_slope_1(p, q), 45.);
-        assert_eq!(calculate_aspect_1(p, q), 180.);
-
-        let (p_prime, q_prime) = calculate_pq_2(&elevations);
-        assert_eq!(calculate_slope_2(d, p_prime, q_prime), 45.);
-        assert_eq!(calculate_aspect_2(p_prime, q_prime), 180.);
+        assert_calculations(&elevations, 45, -1., 0., 45., 180., 31);
+        assert_calculations(&elevations, 90, -0.5, 0., 26.56505, 180., 45);
     }
 
     #[test]
-    fn test_southward_slope() {
-        let d = 45;
+    fn test_slope_facing_north() {
         let elevations = Matrix3x3::new([
             1000, 1000, 1000, 1045, 1045, 1045, 1090, 1090, 1090,
         ]);
 
-        let (p, q) = calculate_pq_1(d, &elevations);
-
-        assert_eq!(p, 0.);
-        assert_eq!(q, 1.);
-        assert_eq!(calculate_slope_1(p, q), 45.);
-        assert_eq!(calculate_aspect_1(p, q), 90.);
-
-        let (p_prime, q_prime) = calculate_pq_2(&elevations);
-        assert_eq!(calculate_slope_2(d, p_prime, q_prime), 45.);
-        assert_eq!(calculate_aspect_2(p_prime, q_prime), 90.);
+        assert_calculations(&elevations, 45, 0., 1., 45., 90., 95);
+        assert_calculations(&elevations, 90, 0., 0.5, 26.56505, 90., 136);
     }
 
     #[test]
-    fn test_northward_slope() {
-        let d = 45;
+    fn test_slope_facing_south() {
         let elevations = Matrix3x3::new([
             1090, 1090, 1090, 1045, 1045, 1045, 1000, 1000, 1000,
         ]);
 
-        let (p, q) = calculate_pq_1(d, &elevations);
-
-        assert_eq!(p, 0.);
-        assert_eq!(q, -1.);
-        assert_eq!(calculate_slope_1(p, q), 45.);
-        assert_eq!(calculate_aspect_1(p, q), 270.);
-
-        let (p_prime, q_prime) = calculate_pq_2(&elevations);
-        assert_eq!(calculate_slope_2(d, p_prime, q_prime), 45.);
-        assert_eq!(calculate_aspect_2(p_prime, q_prime), 270.);
+        assert_calculations(&elevations, 45, 0., -1., 45., 270., 31);
+        assert_calculations(&elevations, 90, 0., -0.5, 26.56505, 270., 45);
     }
 
     #[test]
-    fn test_natural_1() {
-        let d = 45;
+    fn test_natural_slope_sample_1() {
         let elevations = Matrix3x3::new([
             1250, 1256, 1265, 1271, 1280, 1297, 1274, 1303, 1318,
         ]);
 
+        assert_calculations(
+            &elevations,
+            45,
+            0.30833334,
+            0.475,
+            29.52283,
+            57.011475,
+            161,
+        );
+        assert_calculations(
+            &elevations,
+            90,
+            0.15416667,
+            0.2375,
+            15.809441,
+            57.011475,
+            198,
+        );
+    }
+
+    fn assert_calculations(
+        elevations: &Matrix3x3,
+        d: i16,
+        expected_p: f32,
+        expected_q: f32,
+        expected_slope: f32,
+        expected_aspect: f32,
+        expected_igor_hillshading_color: i16,
+    ) {
         let (p, q) = calculate_pq_1(d, &elevations);
 
-        assert_eq!(p, 0.30833334);
-        assert_eq!(q, 0.475);
-        assert_eq!(calculate_slope_1(p, q), 29.52283);
-        assert_eq!(calculate_aspect_1(p, q), 57.011475);
+        assert_eq!(p, expected_p);
+        assert_eq!(q, expected_q);
+
+        let slope = calculate_slope_1(p, q);
+        assert_eq!(slope, expected_slope);
+        let aspect = calculate_aspect_1(p, q);
+        assert_eq!(aspect, expected_aspect);
 
         let (p_prime, q_prime) = calculate_pq_2(&elevations);
-        assert_eq!(calculate_slope_2(d, p_prime, q_prime), 29.52283);
+        assert_eq!(calculate_slope_2(d, p_prime, q_prime), expected_slope);
         assert_eq_approx(
             calculate_aspect_2(p_prime, q_prime),
-            57.011475,
+            expected_aspect,
             0.0001,
         );
+
+        let sun_azimuth = 45;
+        let color = hillshading_1(sun_azimuth, slope as i16, aspect as i16);
+        assert_eq!(color, expected_igor_hillshading_color, "sun_azimuth",);
     }
 }
